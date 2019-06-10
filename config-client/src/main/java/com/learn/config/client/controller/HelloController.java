f289d568998b084bf310ede996a5973e86b8cb75
连培鑫‘ <lianpeixin@sz-sess.com>
2019-06-10 16:02:39 +0800

    配置手动刷新

--- config-client/src/main/java/com/learn/config/client/controller/HelloController.java
+++ config-client/src/main/java/com/learn/config/client/controller/HelloController.java
@@ -1,13 +1,15 @@
-package com.learn.config.client;
+package com.learn.config.client.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class HelloController {
     @Value("${name}")
-    private String name;
+    String name;
     @RequestMapping("/")
     public String hi() {
         return "hi : " + name;
